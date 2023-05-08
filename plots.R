d5bd9e8c765ac49b8ad41d3f32495b91ded5b933
Mathieu Quinodoz <46816306+mquinodo@users.noreply.github.com>
2023-05-08 13:44:04 +0200

    Update plots.R

--- plots.R
+++ plots.R
@@ -502,3 +502,5 @@ if(dim(events)[1]>0){
 
 write.table(events,file=paste(dir,"/0.events.tsv",sep=""),quote=F,row.names=F,sep="\t")
 write.table(phenotype[,c(1,2,4)],file=paste(dir,"/0.phenotypes-all.tsv",sep=""),quote=F,row.names=F,sep="\t")
+
+
